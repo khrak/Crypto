9fe8b35f3d46e08a912db0add452c24bab058fdf
khrak <mamuka_sakhelashvili@yahoo.com>
2016-10-15 00:18:41 +0400

    Encryption process implementation  is done. Needs testing

--- week 2/CBC.py
+++ week 2/CBC.py
@@ -14,7 +14,8 @@ def cbc_encrypt(iv, key, message_text):
     for message_block in message_blocks:
         m = cbc_tools.xor_16_bytes(message_block, cbc_xor_variable)
 
-        cipher_text_matrix = AES.aes_encrypt(key, cbc_tools.get_4_by_4_matrix_from_16_bytes(m))
+        cipher_text_matrix = AES.aes_encrypt(cbc_tools.get_4_by_4_matrix_from_16_bytes(key),
+                                             cbc_tools.get_4_by_4_matrix_from_16_bytes(m))
         cipher_text_block = cbc_tools.get_16byte_array_from_4by4(cipher_text_matrix)
         cipher_text += cipher_text_block
         cbc_xor_variable = cipher_text_block
@@ -22,17 +23,21 @@ def cbc_encrypt(iv, key, message_text):
     return cipher_text
 
 if __name__ == '__main__':
-    message = bytearray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
 
-    print(len(message))
-    message = cbc_tools.add_padd(message)
-    print(message)
-    messages = cbc_tools.get_message_blocks(message)
+    iv = "4ca00ff4c898d61e1edbf1800618fb28"
+    key = "144288afaa485658c611b7f2e574756d"
 
-    print(messages)
+    message = "I am cbc crypto guy!"
 
-    message = cbc_tools.get_4_by_4_matrix_from_16_bytes(messages[0])
+    iv = bytearray.fromhex(iv)
+    iv = [iv[i] for i in range(0, len(iv))]
 
-    print(message)
+    key = bytearray.fromhex(key)
+    key = [key[i] for i in range(0, len(key))]
 
-    print(cbc_tools.get_16byte_array_from_4by4(message))
+    message = message.encode()
+    message = [message[i] for i in range(0, len(message))]
+
+    cipher_text = cbc_encrypt(iv, key, message)
+
+    print(cipher_text)
---
+++ week 2/tools.py
@@ -0,0 +1,4 @@
+def from_hex_to_bytes(hex_string):
+    pass
+    # result = bytearray.fromhex(hex_string)
+    #  string.encode()
